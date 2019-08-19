77b9deae2bb358777b933f7fcf98b5a29ebef74b
Manik Andrey <andre.ivan.manik@gmail.com>
2019-08-19 21:02:02 +0300

    SFW5-50 | #10 | Add base entity with Id | closes #10

--- pet-clinic-data/src/main/java/manik/andrew/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/manik/andrew/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package manik.andrew.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/manik/andrew/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/manik/andrew/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package manik.andrew.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
