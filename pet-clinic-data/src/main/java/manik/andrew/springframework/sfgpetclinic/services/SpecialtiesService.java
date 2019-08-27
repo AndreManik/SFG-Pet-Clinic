0ee050acc1330ff23f6d471c33dfaecd71d05ee7
Manik Andrey <andre.ivan.manik@gmail.com>
2019-08-27 15:41:27 +0300

    SFW5-123 | #31 | Create Speciality Map Service Impl | closes #31

---
+++ pet-clinic-data/src/main/java/manik/andrew/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package manik.andrew.springframework.sfgpetclinic.services;
+
+import manik.andrew.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
