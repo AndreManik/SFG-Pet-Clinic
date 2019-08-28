8a1abac5a219db53a79ce3d7ca057ce825623ebc
Manik Andrey <andre.ivan.manik@gmail.com>
2019-08-28 17:22:54 +0300

    SFW5-155 | #47 | Create Visit Map Service | closes #47

---
+++ pet-clinic-data/src/main/java/manik/andrew/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package manik.andrew.springframework.sfgpetclinic.services;
+
+import manik.andrew.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
